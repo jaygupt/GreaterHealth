48c7a9a9e858c9ee980b8265e23c45365704db25
Jay Gupta <jaygupt1973@gmail.com>
2021-07-17 01:06:58 -0700

    Accessing stateLongName as an ejs variable

--- index.js
+++ index.js
@@ -57,8 +57,18 @@ const userId = 22;
 
 // homepage
 app.get("/", (req, res) => {
+  let stateLongName = { 'AL' : 'Alabama', 'AK' : 'Alaska', 'AZ' : 'Arizona', 'AR' : 'Arkansas', 'CA' : 'California', 'CO' : 'Colorado', 'CT' : 'Connecticut', 'DE' : 'Delaware', 'FL' : 'Florida', 'GA' : 'Georgia', 'HI' : 'Hawaii', 'ID' : 'Idaho', 'IL' : 'Illinois', 'IN' : 'Indiana', 'IA' : 'Iowa', 'KS' : 'Kansas', 
+      'KY' : 'Kentucky', 'LA' : 'Louisiana', 'ME' : 'Maine', 'MD' : 'Maryland', 
+      'MA' : 'Massachusetts', 'MI' : 'Michigan', 'MN' : 'Minnesota', 'MS' : 'Mississippi', 
+      'MO' : 'Missouri', 'MT' : 'Montana', 'NE' : 'Nebraska', 'NV' : 'Nevada', 'NH' : 'New Hampshire', 
+      'NJ' : 'New Jersey', 'NM' : 'New Mexico', 'NY' : 'New York', 'NC' : 'North Carolina', 
+      'ND' : 'North Dakota', 'OH' : 'Ohio', 'OK' : 'Oklahoma', 'OR' : 'Oregon', 'PA' : 'Pennsylvania', 
+      'RI' : 'Rhode Island', 'SC' : 'South Carolina', 'SD' : 'South Dakota', 'TN' : 'Tennessee', 'TX' : 'Texas', 
+      'UT' : 'Utah', 'VT' : 'Vermont', 'VA' : 'Virginia', 'WA' : 'Washington', 'WV' : 'West Virginia', 
+      'WI' : 'Wisconsin', 'WY' : 'Wyoming', 'DC' : 'Washngton DC' };
+
   // full path: views/pages/nationalPage.ejs
-  res.render("pages/nationalPage");
+  res.render("pages/nationalPage", {stateLongName: stateLongName});
 });
 
 app.get("/states/:stateName", (req, res) => {
